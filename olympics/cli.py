96d33c1a38c9f465551f284a9f1cbc999e77b754
Allan Khebab <allan.khebab@edu.igensia.com>
2024-10-10 19:06:51 +0200

    reglage conflit

--- olympics/cli.py
+++ olympics/cli.py
@@ -21,11 +21,7 @@ def top_countries(top=10, file=None):
             str(row['gold']),
             str(row['silver']),
             str(row['bronze']),
-
             str(row['gold'] + row['silver'] + row['bronze']),
-
-            str(row['gold'] + row['silver'] + row['gold']),
-
         )
 
     console = Console(file=file)
